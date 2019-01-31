9b589b3cbca9367a15ce806c9f7a84ae1c4a7113
s777610 <s777610@gmail.com>
2019-01-31 03:38:07 -0800

    implement update comments resolver

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -77,6 +77,24 @@ const Mutation = {
     return post;
   },
 
+  updatePost(parent, args, { db }, info) {
+    const { id, data } = args;
+    const post = db.posts.find(post => post.id === id);
+    if (!post) throw new Error("Post not found");
+    if (typeof data.title === "string") {
+      post.title = data.title;
+    }
+
+    if (typeof data.body === "string") {
+      post.body = data.body;
+    }
+
+    if (typeof data.published === "boolean") {
+      post.published = data.published;
+    }
+    return post;
+  },
+
   deletePost(parent, args, { db }, info) {
     const postIndex = db.posts.findIndex(post => post.id === args.id);
     if (postIndex === -1) throw new Error("Post not found");
@@ -115,6 +133,16 @@ const Mutation = {
     if (commentIndex === -1) throw new Error("Comment not found");
     const deletedComments = db.comments.splice(commentIndex, 1);
     return deletedComments[0];
+  },
+
+  updateComment(parent, args, { db }, info) {
+    const { id, data } = args;
+    const comment = db.comments.find(comment => comment.id === id);
+    if (!comment) throw new Error("Comment not found");
+    if (typeof data.text === "string") {
+      comment.text = data.text;
+    }
+    return comment;
   }
 };
 
